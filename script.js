95d27a904d51fde4932ac62ea0b6b6c682ff562e
Emil Mattsson <emilmattsson14@gmail.com>
2022-09-29 23:11:30 +0200

    bug fixed. Clear data button added so results can be removed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector(".container");
 const submit = document.getElementById("button");
 const input = document.getElementById("input");
 const results = document.querySelector(".results");
+const clear = document.getElementById("clear");
 
 async function getData() {
   const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`;
@@ -18,5 +19,13 @@ async function getData() {
   name.innerText = `Word: ${data[0].word}`;
   definition.innerText = `Definition: ${data[0].meanings[0].definitions[0].definition}`;
   license.innerText = `License name: ${data[0].license.name}`;
+
+  clear.addEventListener("click", function clearData() {
+    results.removeChild(name);
+    results.removeChild(definition);
+    results.removeChild(license);
+  });
+  clearData();
 }
+
 submit.addEventListener("click", getData);
