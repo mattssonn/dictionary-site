1121cb57de52a64785dfa321db93bbd393796e92
Emil Mattsson <emilmattsson14@gmail.com>
2022-09-29 15:19:31 +0200

    Javscript and html 99% finished. Styling needs work

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const getData = async () => {
-  response = await fetch(
-    "https://api.dictionaryapi.dev/api/v2/entries/en/video"
-  );
-  data = await response.json();
-  console.log(data[0].meanings[0].definitions[1].definition);
-  console.log(data[0].word);
+const container = document.querySelector(".container");
+const submit = document.getElementById("button");
+const input = document.getElementById("input");
 
-  const navbar = document.querySelector(".topnav");
+async function getData() {
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input.value}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
   const name = document.createElement("p");
   const license = document.createElement("p");
   const definition = document.createElement("p");
-  navbar.appendChild(name);
-  navbar.appendChild(license);
-  navbar.appendChild(definition);
-  name.innerText = `Name: ${data[0].word}`;
+
+  container.appendChild(name);
+  container.appendChild(license);
+  container.appendChild(definition);
+  name.innerText = `Word: ${data[0].word}`;
   license.innerText = `License name: ${data[0].license.name}`;
   definition.innerText = `Definition: ${data[0].meanings[0].definitions[1].definition}`;
-};
-
-getData();
+}
+submit.addEventListener("click", getData);
